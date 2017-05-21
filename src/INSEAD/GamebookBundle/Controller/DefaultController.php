0b53cc436156f7b369c927f4c418ce75ae232c48
kp <kristen.kod@gmail.com>
2017-05-21 20:04:04 +0200

    create basic views

--- src/INSEAD/GamebookBundle/Controller/DefaultController.php
+++ src/INSEAD/GamebookBundle/Controller/DefaultController.php
@@ -11,13 +11,53 @@ class DefaultController extends Controller
         return $this->render('INSEADGamebookBundle:Default:index.html.twig');
     }
 
+    public function introAction()
+    {
+        return $this->render('INSEADGamebookBundle:Default:intro.html.twig');
+    }
+
     public function chap_1Action()
     {
         return $this->render('INSEADGamebookBundle:Default:chap_1.html.twig');
     }
 
+    public function chap_2Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_2.html.twig');
+    }
+
     public function chap_3Action()
     {
         return $this->render('INSEADGamebookBundle:Default:chap_3.html.twig');
+    }
+
+    public function chap_4Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_4.html.twig');
+    }
+
+    public function chap_5Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_5.html.twig');
+    }
+
+    public function chap_6Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_6.html.twig');
+    }
+
+    public function chap_7Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_7.html.twig');
+    }
+
+    public function chap_8Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_8.html.twig');
+    }
+
+    public function chap_9Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_9.html.twig');
     }
 }
