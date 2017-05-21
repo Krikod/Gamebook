8600948b0f00a93ee4dc5f7c17d464e5de4a44aa
kp <kristen.kod@gmail.com>
2017-05-21 11:20:34 +0200

    first page

--- src/INSEAD/GamebookBundle/Controller/DefaultController.php
+++ src/INSEAD/GamebookBundle/Controller/DefaultController.php
@@ -10,4 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('INSEADGamebookBundle:Default:index.html.twig');
     }
+
+    public function chap_1Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_1.html.twig');
+    }
+
+    public function chap_3Action()
+    {
+        return $this->render('INSEADGamebookBundle:Default:chap_3.html.twig');
+    }
 }
